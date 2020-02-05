d86ddc02c38b09ef1e3899b6fdc3722d16449dd0
Dasha Maletis and Jozy Kinnaman <jozy@>
2020-02-04 20:59:34 -0800

    Add water volume converter~

--- js/sripts.js
+++ js/sripts.js
@@ -22,7 +22,19 @@ var add = function(number1, number2) {
 
     var weight = prompt("What is your weight in lbs?");
     var height = prompt("What is your height in in?");
-    
+
     result = BMI(weight,height).toPrecision(4);
 
   alert("The result is "+ result);
+
+  var volume = prompt("Enter volume...in gallons.")
+
+  var liters = function (volume) {
+      return (volume * 3.78)
+  };
+
+  var result = liters(volume)
+  alert("The volume in liters is..." + result);
+  
+
+  
