adce14af685017af65813e3a6bdc6fff28121f91
Dasha Maletis and Jozy Kinnaman <jozy@>
2020-02-04 20:40:06 -0800

    Add BMI calculator

--- js/sripts.js
+++ js/sripts.js
@@ -1,8 +1,28 @@
 var add = function(number1, number2) {
     return number1 + number2;
   };
-  
+  var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
+
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1, number2));
+
+  var BMI = function(weight,height) {
+    return (weight / (height * height))* 703
+    };
+
+    var weight = prompt("What is your weight in lbs?");
+    var height = prompt("What is your height in in?");
+    
+    result = BMI(weight,height).toPrecision(4);
+
+  alert("The result is "+ result);
